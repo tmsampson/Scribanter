3dad8677a1473a49763115ebab4851dad4587af6
Thomas Sampson <tmsampson@gmail.com>
2025-03-18 17:16:18 +0000

    Fix formatting in RunScriban method to ensure consistent output suppression

--- Src/Scribanter.Test/Helpers.cs
+++ Src/Scribanter.Test/Helpers.cs
@@ -54,7 +54,7 @@ public class TestBase
 
 	public void RunScriban(int expectedExitCode, string[] commandLineArgs)
 	{
-		//using (SuppressOutput())
+		using (SuppressOutput())
 		{
 			int exitCode = Program.Main(commandLineArgs);
 			Assert.Equal(expectedExitCode, exitCode);
